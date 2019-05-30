caa74ce24aedb9ee338b4cb1404beb529910d1cd
Jason Zhao <jasonjinzhao99@gmail.com>
2019-05-29 19:18:53 -0700

    chest data

--- utils/adapt_data.py
+++ utils/adapt_data.py
@@ -15,7 +15,7 @@ def batch_fill(testx, batch_size):
     fill = np.ones(new_shape)
     return np.concatenate([testx[ran_from:ran_to], fill], axis=0), size
 
-def adapt_labels_outlier_task(true_labels, label):
+def adapt_labels_outlier_task(true_labels, abnormal_list):
     """Adapt labels to anomaly detection context
 
     Args :
@@ -24,11 +24,7 @@ def adapt_labels_outlier_task(true_labels, label):
     Returns :
             true_labels (list): list of labels, 1 for anomalous and 0 for normal
     """
-    if label == 1:
-        (true_labels[true_labels == label], true_labels[true_labels != label]) = (1, 0)
-        true_labels = [1] * true_labels.shape[0] - true_labels
-    else:
-        (true_labels[true_labels != label], true_labels[true_labels == label]) = (1, 0)
+    (true_labels[true_labels in abnormal_list], true_labels[true_labels not in abnormal_list]) = (1, 0)
     return true_labels
 
 
