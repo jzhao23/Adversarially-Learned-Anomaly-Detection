7917c6372452392bb2977c60f0b7c651afa512a9
Jason Zhao <jasonjinzhao99@gmail.com>
2019-05-31 10:29:12 -0700

    Update chest.py

--- data/chest.py
+++ data/chest.py
@@ -69,6 +69,49 @@ def _get_adapted_dataset(split, label, centered, normalize):
     return (dataset[key_img], dataset[key_lbl])
 
 def _load_holdout_dataset(abnormal_list=[]):
+    
+    """# load data
+    X, Y = utils.load_X_and_Y()
+    x_train, x_dev, x_test = X
+    y_train, y_dev, y_test = Y
+
+    # holdout training and dev data if requested
+    X_descr = utils.load_X_descr()
+    x_train_all_descr, x_dev_all_descr, x_test_descr = X_descr
+
+    include_x_train = []
+    include_y_train = []
+    for idx in range(len(x_train)):
+        if x_train_all_descr[idx] not in abnormal_list:
+            include_x_train.append(x_train[idx])
+            include_y_train.append(y_train[idx])
+
+    include_x_dev = []
+    include_y_dev = []
+    for idx in range(len(x_dev)):
+        if x_dev_all_descr[idx] not in abnormal_list:
+            include_x_dev.append(x_dev[idx])
+            include_y_dev.append(y_dev[idx])
+    
+    x_train = np.array(include_x_train).reshape((-1, 224, 224, 3))
+    y_train = np.array(include_y_train).reshape((-1, 1))
+    y_train = np.array(np.zeros(y_train.shape))
+
+    x_dev = np.array(include_x_dev).reshape((-1, 224, 224, 3))
+    y_dev = np.array(include_y_dev).reshape((-1, 1))
+    y_dev = np.array(np.zeros(y_dev.shape))
+
+    for i in range(len(y_test)):
+        if x_test_descr[i] in abnormal_list:
+            y_test[i] = 1
+        else:
+            y_test[i] = 0
+
+    print("average value of  train label: ", np.average(y_train))
+    print("average value of  dev label: ", np.average(y_dev))
+    print("average value of  test label: ", np.average(y_test))
+    return (x_train, x_dev, x_test, y_train, y_dev, y_test)"""
+
     # load data
     X, Y = utils.load_X_and_Y()
     x_train, x_dev, x_test = X
@@ -78,33 +121,47 @@ def _load_holdout_dataset(abnormal_list=[]):
     X_descr = utils.load_X_descr()
     x_train_all_descr, x_dev_all_descr, x_test_descr = X_descr
 
+    include_x_train = []
+    include_y_train = []
     holdout_x_train = []
     holdout_y_train = []
     for idx in range(len(x_train)):
         if x_train_all_descr[idx] not in abnormal_list:
+            include_x_train.append(x_train[idx])
+            include_y_train.append(y_train[idx])
+        else:
             holdout_x_train.append(x_train[idx])
-            holdout_y_train.append(y_train[idx])
+            holdout_y_train.append(1)
 
+    include_x_dev = []
+    include_y_dev = []
     holdout_x_dev = []
     holdout_y_dev = []
     for idx in range(len(x_dev)):
         if x_dev_all_descr[idx] not in abnormal_list:
+            include_x_dev.append(x_dev[idx])
+            include_y_dev.append(y_dev[idx])
+        else:
             holdout_x_dev.append(x_dev[idx])
-            holdout_y_dev.append(y_dev[idx])
+            holdout_y_dev.append(1)
     
-    x_train = np.array(holdout_x_train).reshape((-1, 224, 224, 3))
-    y_train = np.array(holdout_y_train).reshape((-1, 1))
+    x_train = np.array(include_x_train).reshape((-1, 224, 224, 3))
+    y_train = np.array(include_y_train).reshape((-1, 1))
     y_train = np.array(np.zeros(y_train.shape))
 
-    x_dev = np.array(holdout_x_dev).reshape((-1, 224, 224, 3))
-    y_dev = np.array(holdout_y_dev).reshape((-1, 1))
+    x_dev = np.array(include_x_dev).reshape((-1, 224, 224, 3))
+    y_dev = np.array(include_y_dev).reshape((-1, 1))
     y_dev = np.array(np.zeros(y_dev.shape))
 
+    # add excluded abnormalities (train/dev) to test set!
     for i in range(len(y_test)):
-        if x_test_descr[i] in abnormal_list:
+        if x_train_all_descr[i] in abnormal_list:
             y_test[i] = 1
         else:
             y_test[i] = 0
+
+    x_test += holdout_x_train + holdout_x_dev
+    y_test += holdout_y_train + holdout_y_dev
 
     print("average value of  train label: ", np.average(y_train))
     print("average value of  dev label: ", np.average(y_dev))
